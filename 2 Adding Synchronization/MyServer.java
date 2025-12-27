1eb05b21a15f70a5c3fea07b861cbe76c8adc9ce
Harshana Amuwatte <harshanaamuwatte@gmail.com>
2025-12-28 01:12:27 +0530

    Implement ClientHandler and MyServer classes for multithreaded server with synchronization

---
+++ 2 Adding Synchronization/MyServer.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class MyServer{
+    public static int count = 0;
+    public static void main(String[] args) throws IOException{
+        
+        ServerSocket server = new ServerSocket(5000);
+        while(true){
+            Socket client =server.accept();
+            ClientHandler handler = new ClientHandler(client);
+            Thread t = new Thread(handler); 
+            t.start();
+            
+        }
+
+    }
+}
